6181cb7543aeb7a21ca0969b84d53ebb84c432a7
J-minkuk <mingood92@gmail.com>
2021-01-19 02:19:02 +0900

    RefreshScope

--- product-api/src/main/java/com/hardy/ex/controller/ProductController.java
+++ product-api/src/main/java/com/hardy/ex/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.hardy.ex.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Hardy(조민국) / mingood92@gmail.com
  * @since 2021. 01. 18
  */
+@RefreshScope
 @RestController
 @RequestMapping("product")
 public class ProductController {
+
+    @Value("${test.value}")
+    private String testValue;
 
     @GetMapping("/ping")
     public String getProduct() {
@@ -23,6 +29,11 @@ public class ProductController {
 //            e.printStackTrace();
 //        }
         return "Product API";
+    }
+
+    @GetMapping("/value")
+    public String getTestValue() {
+        return testValue;
     }
 
 }
